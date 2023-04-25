ad4129d97087ebfe6f8373553ea2431c85e4884b
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-26 02:36:37 +0400

    List

---
+++ List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+using System.Text.Encodings.Web;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<Programinglanguage> languages = new List<Programinglanguage>();
+            languages.Add(new Programinglanguage { Name = "C#", Year = 2001 });
+            languages.Add(new Programinglanguage { Name = "C++", Year = 1983 });
+            languages.Add(new Programinglanguage { Name = "Java", Year = 1995 });
+            languages.Add(new Programinglanguage { Name = "Javascript", Year = 1996 });
+            languages.Add(new Programinglanguage { Name = "C", Year = 1972 });
+
+
+            languages.Sort((a, b) => a.Year.CompareTo(b.Year));
+
+            foreach (var item in languages)
+            {
+                Console.WriteLine(item.Name + " " + item.Year);
+            }
+        }
+    }
+}
---
+++ List/Programinglanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class Programinglanguage
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+      
+    }
+}
